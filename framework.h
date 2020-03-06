554385ee12410ef85b2012a25a1cc45f275d306d
zdanielz <43722126+zdanielz@users.noreply.github.com>
2020-03-06 14:36:15 +0300

    Add files via upload

---
+++ framework.h
@@ -0,0 +1,14 @@
+﻿// header.h: включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта
+//
+
+#pragma once
+
+#include "targetver.h"
+// Файлы заголовков Windows
+#include <windows.h>
+// Файлы заголовков среды выполнения C
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
